98d3b69ab7cec9aa649696446cd638e23ada4b63
sdmotto <119888788+sdmotto@users.noreply.github.com>
2023-11-30 13:20:41 -0600

    basic home page

--- app.py
+++ app.py
@@ -8,8 +8,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # TODO populate with all titles
+    titles = []
+    return render_template("index.html", book_titles=jsonify(titles).json)
 
+@app.route("/recommendations", methods=["GET"])
+def recommendations():
+    # TODO get recommendations based off description and reviews
+    recommendations = []
+    return render_template("recommendations.html", recommendations=jsonify(recommendations).json)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8888, debug=True)
