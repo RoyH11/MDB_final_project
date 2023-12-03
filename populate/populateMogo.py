346bac005697fd8770a286c52472fb476502b436
Roy Huang <98927448+RoyH11@users.noreply.github.com>
2023-12-02 19:32:17 -0600

    connection error

---
+++ populate/populateMogo.py
@@ -0,0 +1,42 @@
+from pymongo import MongoClient
+import pandas as pd
+
+def import_data(collection_name, my_csv_file_path):
+
+    # Set up MongoDB connection
+    client = MongoClient("mongodb://localhost:27017")
+    db = client["books"]
+    collection = db[collection_name]
+
+    # Specify the CSV file path
+    csv_file_path = my_csv_file_path
+
+    # Set the batch size
+    batch_size = 50
+
+    # Line count
+    line_count = 0
+
+    # Read CSV file in chunks and insert data into MongoDB in batches
+    for chunk in pd.read_csv(csv_file_path, chunksize=batch_size):
+        # Convert the chunk to a list of dictionaries (each row becomes a dictionary)
+        data = chunk.to_dict(orient="records")
+
+        # Increment line count
+        line_count += len(data)
+
+        # Insert data into MongoDB
+        collection.insert_many(data)
+
+        # Print progress
+        print(f"Processed {line_count} lines.")
+        
+    print(f"{csv_file_path} imported successfully.")
+
+
+
+
+if __name__ == "__main__":
+    import_data("books_data", "data/books_data.csv")
+    import_data("books_rating", "data/Books_rating.csv")
+
