610bdfc89c6df8ce9f8d3d27ecd16de8c2968dfe
sdmotto <119888788+sdmotto@users.noreply.github.com>
2023-12-04 12:06:22 -0600

    basic full text search on description

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 from flask import Flask, request, jsonify, render_template
+from models import MongoModel, Neo4jModel, RedisModel
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    # TODO populate with all titles
-    titles = []
+    mongoModel = MongoModel()
+
+    # this is horribly inefficent, it finds all book titles
+    # then passes it to the page every time
+    titles = mongoModel.get_all_titles()
+    mongoModel.close()
+
     return render_template("index.html", book_titles=jsonify(titles).json)
 
 @app.route("/recommendations", methods=["GET"])
 def recommendations():
+    mongoModel = MongoModel()
     title = request.args.get("title")
-    # TODO get recommendations based off description and reviews
-    recommendations = []
+    recommendations = mongoModel.get_recommendations(title)
+    mongoModel.close()
+
     return render_template("recommendations.html", recommendations=jsonify(recommendations).json)
 
 if __name__ == "__main__":
+    #mongoModel = MongoModel()
+    #mongoModel.create_text_index()
     app.run(host='127.0.0.1', port=8888, debug=True)
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import pymongo
 import neo4j
 import redis
+from bson.son import SON
 
 class MongoModel:
     def __init__(self, url="mongodb://localhost:27017"):
@@ -9,6 +10,51 @@ class MongoModel:
 
     def close(self):
         self.client.close()
+
+    def get_all_titles(self):
+        titles = []
+        database = self.client["books"]
+        collection = database["books"]
+
+        documents = collection.find()
+
+        for document in documents:
+            title = document.get("Title")
+            if title:
+                titles.append(title)
+
+        return titles
+    
+    def create_text_index(self):
+        database = self.client["books"]
+        collection = database["books"]
+        # Create a text index on the "description" field if it doesn't exist
+        index_info = collection.index_information()
+        if "description_text" not in index_info:
+            collection.create_index([("description", "text")], name="description_text")
+    
+    def get_recommendations(self, title):
+        # Find the document for the provided title
+        query = {"Title": title}
+        database = self.client["books"]
+        collection = database["books"]
+
+        target_book = collection.find_one(query)
+
+        if target_book:
+            # Use full-text search to find similar books based on the description
+            search_result = collection.find(
+                {"$text": {"$search": target_book['description']}},
+                {"score": {"$meta": "textScore"}}
+            ).sort([("score", {"$meta": "textScore"})]).limit(6)
+
+            # Extract recommended titles
+            recommended_titles = [book["Title"] for book in search_result if book["_id"] != target_book["_id"]]
+
+            return recommended_titles
+        else:
+            print("damn")
+            return []
 
 
 class Neo4jModel:
